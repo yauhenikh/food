45bde8e5769f45370c9403e074644f1a2809bf2e
Yauheni Khotsin <yauheni_khotsin@epam.com>
2020-05-13 17:46:05 +0300

    Add more styling to Search Bar.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,7 +7,7 @@ const SearchBar = () => {
     <View style={styles.backgroundStyle}>
       <Feather
         name="search"
-        size={30}
+        style={styles.iconStyle}
       />
       <TextInput
         style={styles.inputStyle}
@@ -19,6 +19,7 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
   backgroundStyle: {
+    marginTop: 15,
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
@@ -26,9 +27,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   inputStyle: {
-    borderColor: 'black',
-    borderWidth: 1,
-    flex: 1
+    flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15
   }
 });
 
