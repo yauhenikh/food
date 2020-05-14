87a63edd2b72f590dcec8e27c7b226aa5c1ff261
Yauheni Khotsin <yauheni_khotsin@epam.com>
2020-05-14 18:56:12 +0300

    Make the request.

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
-    Authorization: 'Bearer xZQofDOnNnZ1EYwUrvBMrM2w-328nmHedXbDzo9jou7SWf14wh6Wthk7QYkkMbYoByZDq_9QS3uxwqOARBHsWObAzq1ZsereDR4rU8PitY-NH_SFlzadUAuGjNS6XnYx'
+    Authorization:
+     'Bearer xZQofDOnNnZ1EYwUrvBMrM2w-328nmHedXbDzo9jou7SWf14wh6Wthk7QYkkMbYoByZDq_9QS3uxwqOARBHsWObAzq1ZsereDR4rU8PitY-NH_SFlzadUAuGjNS6XnYx'
   }
 });
