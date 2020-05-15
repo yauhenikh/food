103608665343b6938be5adc6fc4a54147847ff48
Yauheni Khotsin <yauheni_khotsin@epam.com>
2020-05-15 15:26:37 +0300

    Add useEffect hook.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -22,6 +22,10 @@ const SearchScreen = () => {
       setErrorMessage('Something went wrong');
     }
   };
+
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
 
   return (
     <View>
