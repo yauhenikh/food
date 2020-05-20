bcd54400ca36ba5ed3642ac518e35a86cc5492af
Yauheni Khotsin <yauheni_khotsin@epam.com>
2020-05-20 16:10:15 +0300

    Show search results. Group results. Render FlatList.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,10 +2,17 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = (price) => {
+    return results.filter(result => {
+      return result.price === price;
+    });
+  };
 
   return (
     <View>
@@ -20,6 +27,18 @@ const SearchScreen = () => {
          : null
       }
       <Text>We have found {results.length} results</Text>
+      <ResultsList
+        results={filterResultsByPrice('$')}
+        title="Cost Effective"
+      />
+      <ResultsList
+        results={filterResultsByPrice('$$')}
+        title="Bit Pricier"
+      />
+      <ResultsList
+        results={filterResultsByPrice('$$$')}
+        title="Big Spender"
+      />
     </View>
   );
 };
